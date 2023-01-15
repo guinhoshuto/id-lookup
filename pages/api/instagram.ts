5077b2285c620a96c17b145052b73d1f2dcda989
Guilherme S <guinho.shuto@gmail.com>
2023-01-14 23:48:56 -0400

    instagram token

---
+++ pages/api/instagram.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const url = 'https://graph.facebook.com/v15.0/' + process.env.FACEBOOK_ID;
+
+ "fields=business_discovery.username(alok)%7Bid%2Cprofile_picture_url%2Cname%2Cwebsite%2Cfollowers_count%2Cfollows_count%2Cmedia_count%2Cmedia.fields(like_count%2Ccomments_count%2Cmedia_url%2Cmedia_type%2Ctimestamp)%7D&access_token="
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if(req.method === 'POST'){
+    const data = await axios.get(`${url}?fields=business_discovery.username(${req.body.params})%7Bid%2Cprofile_picture_url%2Cname%2Cwebsite%2Cfollowers_count%2Cfollows_count%2Cmedia_count%2Cmedia.fields(like_count%2Ccomments_count%2Cmedia_url%2Cmedia_type%2Ctimestamp)%7D&access_token=${process.env.FACEBOOK_TOKEN}`)
+  } else {
+    res.status(405).json({ msg: 'VISH' })
+  }
+
+}
+``
