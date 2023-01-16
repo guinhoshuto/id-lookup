5acf690e58b3e459463a1e00905a44f9dcfd307e
Guilherme S <guinho.shuto@gmail.com>
2023-01-15 22:29:42 -0400

    gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({gtmId: 'GTM-MCZK4R7'})
+  }, [])
   return <Component {...pageProps} />
 }
