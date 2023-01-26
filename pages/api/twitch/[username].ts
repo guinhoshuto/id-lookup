1825280e34f68916ebc091ea5af7d18ddd2be987
Guilherme Shuto <guinho.shuto@gmail.com>
2023-01-26 17:50:19 -0400

    limpezas

--- pages/api/twitch/[username].ts
+++ pages/api/twitch/[username].ts
@@ -14,32 +14,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  // console.log(req.body.username)
-  // if(req.method === 'POST'){
-  //   try {
-  //     const username = await twitchReq.get(url + req.body.username)
-  //     console.log(username.data)
-  //     res.status(200).json( username.data.data[0])
-  //   } catch(e) {
-  //     console.log(e)
-  //     res.status(500).json({'msg': e})
-  //   }
-  // } else {
-    const query = req.query
-    console.log(query)
-    try {
-      // const username = await axios.get(url+query.username, headers)
-      // console.log(headers.headers)
-      const username = await twitchReq.get(url+query.username)
-      // const username = await axios.get('https://api.feras.club/team/search/' + query.username)
-      console.log(username.data.data[0])
-      console.log(url+query.username)
-      res.status(200).json( username.data.data[0])
-    // res.status(405).json({ msg: req.method })
-    } catch(e){
-      console.log(e)
-      res.status(500).json({'msg': e})
-    }
-  // }
+  const query = req.query
+  console.log(query)
+  try {
+    const username = await twitchReq.get(url+query.username)
+    console.log(username.data.data[0])
+    console.log(url+query.username)
+    res.status(200).json( username.data.data[0])
+  } catch(e){
+    console.log(e)
+    res.status(500).json({'msg': e})
+  }
 
 }
