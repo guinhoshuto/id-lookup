13556ac723a5e50863babfb47c1ab119c80ec2a6
Guilherme S <guinho.shuto@gmail.com>
2023-01-25 20:20:28 -0400

    troca de link pra Link e renomeado notfound

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Header(){
     return(
         <div className="w-full h-8 flex justify-between py-8 px-12 mb-8 ">
@@ -5,8 +7,8 @@ export default function Header(){
                 ID LOOKUP
             </div>
             <div className="flex gap-6">
-                <a href="/twitch">Twitch ID</a>
-                <a href="/instagram">Instagram ID</a>
+                <Link href="/twitch">Twitch ID</Link>
+                <Link href="/instagram">Instagram ID</Link>
             </div>
         </div>
     )
--- components/notFound.tsx
+++ components/notFound.tsx
@@ -1,4 +1,4 @@
-export default function notFound(){
+export default function NotFound(){
     return(
         <div className="text-center m-8">
             Username not found :(
