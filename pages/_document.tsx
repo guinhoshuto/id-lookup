edb8dbefca40ac70a3bdbf9d36cf26be3a8951ca
Guilherme S <guinho.shuto@gmail.com>
2023-01-28 19:41:06 -0400

    adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5577466732553898"
+     crossOrigin="anonymous"></script>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
 <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200;400&display=swap" rel="stylesheet"></link>
